2de05ab6c6b0f8aeec4120d0179981b9a1e30534
KostadinDev <48786151+KostadinDev@users.noreply.github.com>
2020-05-07 12:27:37 -1000

    Updates

---
+++ precache-manifest.9fc8e3e8c2d7424470c061e067d70b85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9533bae05cb5457364d0e5e782f29714",
+    "url": "/holographic-fan/index.html"
+  },
+  {
+    "revision": "d6ee5e4742a5947a9198",
+    "url": "/holographic-fan/static/css/main.c9b0e763.chunk.css"
+  },
+  {
+    "revision": "aaea0888b8e9d6f88b85",
+    "url": "/holographic-fan/static/js/2.6278bbef.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/holographic-fan/static/js/2.6278bbef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6ee5e4742a5947a9198",
+    "url": "/holographic-fan/static/js/main.aab5343a.chunk.js"
+  },
+  {
+    "revision": "ada7df7176c00491858a",
+    "url": "/holographic-fan/static/js/runtime-main.6e8cf49f.js"
+  },
+  {
+    "revision": "07615a857d8425b612eca1ba08d18811",
+    "url": "/holographic-fan/static/media/ComputerScience-no-background-pc.07615a85.png"
+  }
+]);
